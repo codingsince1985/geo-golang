580e43e29781714b02e210902d2317b19c5f3301
Lucas Alcantara <lucas.alcantara-freire@sixt.com>
2019-03-14 16:51:41 +0100

    print payload when an error is received in response method

--- http_geocoder.go
+++ http_geocoder.go
@@ -135,6 +135,7 @@ func response(ctx context.Context, url string, obj ResponseParser) error {
 		return nil
 	}
 	if err := json.Unmarshal([]byte(body), obj); err != nil {
+		Logger.Printf("payload: %s", body)
 		return err
 	}
 
